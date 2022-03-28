caf75af82390d0c14179d21b9719a53d3e3b073b
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-03-28 10:22:36 +0300

    Рефакторинг

--- Laba_2_1_full/Program.cs
+++ Laba_2_1_full/Program.cs
@@ -21,6 +21,7 @@ namespace Laba_2_1_full
                 Console.Write($"{array[i]} ");
             }
             Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------");
         }
 
         static int[] ArrayCopy(int[] array)
@@ -33,7 +34,7 @@ namespace Laba_2_1_full
             return newArr;
         }
 
-        static void BubbleSort(int[] mas)
+        static void BubbleSort(int[] mas, ref int count_comparer, ref int count_move)
         {
             int temp;
             int n = mas.Length;
@@ -41,44 +42,53 @@ namespace Laba_2_1_full
             {
                 for (int j = 0; j < n - 1- i; j++)
                 {
+                    count_comparer += 1;
                     if (mas[j] > mas[j + 1])
                     {
                         temp = mas[j];
                         mas[j] = mas[j + 1];
                         mas[j + 1] = temp;
+                        count_move++;
                     }
                 }
             }
         }
 
-        static void SelectionSort(int[] array)
+        static void SelectionSort(int[] array, ref int count_comparer, ref int count_move)
         {
             int temp;
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.Length - 1; i++)
             {
                 int min = i;
                 for (int j = i + 1; j < array.Length; j++)
                 {
+                    count_comparer += 1;
                     if (array[j] < array[min])
                         min = j;
                 }
+                if (array[min] != array[i])
+                    count_move++;
                 temp = array[min];
                 array[min] = array[i];
                 array[i] = temp;
             }
         }
 
-        static void InsertionSort(int[] array)
+        static void InsertionSort(int[] array, ref int count_comparer, ref int count_move)
         {
-            for (int i = 1; i < array.Length; i++)
+            for (int i = 1; i < array.Length - 1; i++)
             {
                 int x = array[i];
                 int j = i;
+                
                 while (j > 0 && array[j - 1] > x)
                 {
+                    count_comparer++;
+                    count_move++;
                     array[j] = array[j - 1];
                     j -= 1;
                 }
+                count_comparer += 1;
                 array[j] = x;
             }
         }
@@ -88,11 +98,14 @@ namespace Laba_2_1_full
             bool result = true;
             int[] array = null;
             int[] array_sort;
+            
 
             var watch = new Stopwatch();
 
             while (result)
             {
+                int count_comparer = 0;
+                int count_move = 0;
                 Console.WriteLine("Выберите действие:");
                 Console.WriteLine("1. Создать массив.");
                 Console.WriteLine("2. Сортировка обменом (метод пузырька).");
@@ -114,15 +127,18 @@ namespace Laba_2_1_full
                         {
                             array = new int[size];
                             ArrayFill(array);
+                            Console.WriteLine("Исходный массив");
+                            ArrayPrint(array);
                         }
                         break;
                     case 2:
                         array_sort = ArrayCopy(array);
                         watch.Start();
-                        BubbleSort(array_sort);
+                        BubbleSort(array_sort, ref count_comparer, ref count_move);
                         watch.Stop();
                         Console.WriteLine("Пузырьковая сортирвока:");
-                        Console.WriteLine($"Потраченное время: {watch.ElapsedTicks}");
+                        Console.WriteLine($"Колличество сравнений: {count_comparer}");
+                        Console.WriteLine($"Колличество перестановок {count_move}");
                         ArrayPrint(array_sort);
                         watch.Reset();
                         break;
@@ -130,20 +146,22 @@ namespace Laba_2_1_full
                     case 3:
                         array_sort = ArrayCopy(array);
                         watch.Start();
-                        SelectionSort(array_sort);
+                        SelectionSort(array_sort, ref count_comparer, ref count_move);
                         watch.Stop();
                         Console.WriteLine("Сортировка выборкой:");
-                        Console.WriteLine($"Потраченное время: {watch.ElapsedTicks}");
+                        Console.WriteLine($"Колличество сравнений: {count_comparer}");
+                        Console.WriteLine($"Колличество перестановок {count_move}");
                         ArrayPrint(array_sort);
                         watch.Reset();
                         break;
                     case 4:
                         array_sort = ArrayCopy(array);
                         watch.Start();
-                        InsertionSort(array_sort);
+                        InsertionSort(array_sort, ref count_comparer, ref count_move);
                         watch.Stop();
                         Console.WriteLine("Сортировка вставками:");
-                        Console.WriteLine($"Потраченное время: {watch.ElapsedTicks}");
+                        Console.WriteLine($"Колличество сравнений: {count_comparer}");
+                        Console.WriteLine($"Колличество перестановок {count_move}");
                         ArrayPrint(array_sort);
                         watch.Reset();
                         break;
